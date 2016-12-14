d8e6e1f0046f4eff48bd745b5cc4ff150dc6f235
cbathgate <c.bathgate1@gmail.com>
2016-12-14 15:34:13 -0800

    queue for functional classpattern passes.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var key = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.dequeue = function() {
+    var first = storage[0];
+    for (var i in storage) {
+      if (i === '0') {
+        continue;
+      } else {
+        storage[parseInt(i) - 1] = storage[i];  
+      }
+    } 
+    if (key > 0) {
+      key--;
+    }
+    return first;
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
