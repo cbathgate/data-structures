8ba35695a8790c9d2903649c29e3939e2b59684d
cbathgate <c.bathgate1@gmail.com>
2016-12-14 15:12:45 -0800

    Stack functional pattern passes tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.pop = function() {
+    var last = storage[key - 1];
+    storage[key - 1] = undefined;
+    if ( key > 0) {
+      key--;
+    }
+    return last;
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
 };
+
+//
