c47782a014509bc010ca1b1ff0b9975949efb2cb
cbathgate <c.bathgate1@gmail.com>
2016-12-14 16:27:55 -0800

    the functional-shared pattern stack tests passes.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  key: 0,
+  size: function () {   
+    return this.key;
+  },
+  push: function (value) {
+    this[this.key] = value;
+    this.key++;          
+  },
+  pop: function () {
+    var last = this[this.key - 1];
+    if (this.key > 0 ) {
+      delete this[this.key - 1]; 
+      this.key--;
+    }
+    return last;
+  }
+};
 
 
