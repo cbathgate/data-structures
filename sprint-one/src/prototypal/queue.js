2ff388b2296044c540519fa763b0253a9e25a6e6
cbathgate <c.bathgate1@gmail.com>
2016-12-14 20:39:53 -0800

    queue.js protypal pattern tests pass

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  key: 0,
+  size: function () {
+    return this.key;
+  },
+  enqueue: function (value) {
+    this[this.key] = value;
+    this.key++;
+  },
+  dequeue: function () {
+    var first = this[0];
+    for (var i in this) {
+      if (i === '0') {
+        continue;
+      } else {
+        this[parseInt(i) - 1] = this[i];
+      }
+    }
+    if (this.key > 0) {
+      delete this[this.key - 1]; 
+      this.key--;
+    } 
+    return first;
+  }
+};
 
 
