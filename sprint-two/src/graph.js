fa5b6acc1d4ad4a21d92ff8716bf9c0ac0bb1a8d
cbathgate <c.bathgate1@gmail.com>
2016-12-16 11:40:39 -0800

    graph.js tests pass

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,35 +1,68 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function() { 
+  this.nodes = {}; 
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(node) {  
+  this.nodes[node] = node;
+
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var key in this.nodes) {
+    if (this.nodes[key] === node ) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.nodes[node];
+  for (var key in this.edges) {
+    if (this.edges[key][0] === node || this.edges[key][1] === node) {
+      this.removeEdge(this.edges[key][0], this.edges[key][1]);
+    }
+  }
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var key in this.edges) {
+    if (key === fromNode + ' ' + toNode) {
+      return true;
+    }
+    if (key === toNode + ' ' + fromNode) {
+      return true;
+    }    
+  }
+  return false;
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode + ' ' + toNode] = [fromNode, toNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.edges[fromNode + ' ' + toNode];
+  delete this.edges[toNode + ' ' + fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodes) {
+    cb(this.nodes[key]);
+  }
 };
 
 /*
